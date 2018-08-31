b7734f05429b6e213ab04abce6cc4b5268e4174a
giDai7ja <36735901+giDai7ja@users.noreply.github.com>
2018-08-31 16:29:09 +0300

    BugFixes

--- Cool/Fonariki/Fonariki.ino
+++ Cool/Fonariki/Fonariki.ino
@@ -22,86 +22,26 @@
 
 byte F[4] = {P0, P1, DRL0, DRL1};
 
-//double tmp;
-
-byte StepTurn = 0;
-byte TurnIndex = 0;
-unsigned long TurnTime;
 byte TurnDelay;
-
-byte StepAuto = 0;
-unsigned long AutoTime;
-
-byte StepDRL = 0;
-byte DRLPWM = 255;
-byte RealPWM = 255;
-unsigned long DRLTime = 0;
-
-byte StepPWM = 0;
-unsigned long PWMTime = 0;
-
-byte StepEff = 0;
-
 byte StepStrobe = 0;
+byte StepShow = 0;
+byte StepPWM = 0;
+
+unsigned long DRLTime = 0;
 unsigned long TimeStrobe = 0;
 
-byte StepShow = 0;
-unsigned long ShowTime = 0;
+//double tmp;
 
-byte TURN_0[80] = {
-  0b00000000, 0b00000001,
-  0b00000000, 0b00000011,
-  0b00000000, 0b00000111,
-  0b00000000, 0b00001111,
-  0b00000000, 0b00011111,
-  0b00000000, 0b00111111,
-  0b00000000, 0b01111111,
-  0b00000000, 0b11111111,
-  0b00000001, 0b11111111,
-  0b00000011, 0b11111111,
-  0b00000111, 0b11111111,
-  0b00001111, 0b11111111,
-  0b00011111, 0b11111111,
-  0b00111111, 0b11111111,
-  0b01111111, 0b11111111,
-  0b11111111, 0b11111111,
-
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-  0b11111111, 0b11111111,
-
-  0b11111111, 0b11111110,
-  0b11111111, 0b11111100,
-  0b11111111, 0b11111000,
-  0b11111111, 0b11110000,
-  0b11111111, 0b11100000,
-  0b11111111, 0b11000000,
-  0b11111111, 0b10000000,
-  0b11111111, 0b00000000,
-  0b11111110, 0b00000000,
-  0b11111100, 0b00000000,
-  0b11111000, 0b00000000,
-  0b11110000, 0b00000000,
-  0b11100000, 0b00000000,
-  0b11000000, 0b00000000,
-  0b10000000, 0b00000000,
-  0b00000000, 0b00000000
-};
-
+// ----- Инициализация
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(19200);
   SPI.begin();
   //  SPI.setClockDivider(SPI_CLOCK_DIV128);
 
-  pinMode(PWM0, OUTPUT);
   digitalWrite(PWM0, HIGH);
-  pinMode(PWM1, OUTPUT);
+  pinMode(PWM0, OUTPUT);
   digitalWrite(PWM1, HIGH);
+  pinMode(PWM1, OUTPUT);
 
   pinMode(P0, OUTPUT);
   digitalWrite(P0, LOW);
@@ -128,7 +68,11 @@ void setup() {
   if ( TurnDelay == 255 ) TurnDelay = 20;
   DRLTime = millis() + 200;
 }
+// ----- Конец инициализации
 
+
+
+// ----- Главный цикл
 void loop() {
   TaskTurn();
   TaskAuto();
@@ -136,6 +80,8 @@ void loop() {
   TaskPWM();
   TaskEffects();
 }
+// ----- Конец главного цикла
+
 
 // Подпрограмма записи в регистр
 void SendLED( byte A, byte D) {
@@ -143,7 +89,66 @@ void SendLED( byte A, byte D) {
   SPI.transfer(D);
   digitalWrite(A, HIGH);
 }
+// ----- Конец записи
 
+
+
+// ---------- Эффект поворотника
+byte TURN_0[96] = {
+  0b00000000, 0b00000001,
+  0b00000000, 0b00000011,
+  0b00000000, 0b00000111,
+  0b00000000, 0b00001111,
+  0b00000000, 0b00011111,
+  0b00000000, 0b00111111,
+  0b00000000, 0b01111111,
+  0b00000000, 0b11111111,
+  0b00000001, 0b11111111,
+  0b00000011, 0b11111111,
+  0b00000111, 0b11111111,
+  0b00001111, 0b11111111,
+  0b00011111, 0b11111111,
+  0b00111111, 0b11111111,
+  0b01111111, 0b11111111,
+  0b11111111, 0b11111111,
+
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+  0b11111111, 0b11111111,
+
+  0b11111111, 0b11111110,
+  0b11111111, 0b11111100,
+  0b11111111, 0b11111000,
+  0b11111111, 0b11110000,
+  0b11111111, 0b11100000,
+  0b11111111, 0b11000000,
+  0b11111111, 0b10000000,
+  0b11111111, 0b00000000,
+  0b11111110, 0b00000000,
+  0b11111100, 0b00000000,
+  0b11111000, 0b00000000,
+  0b11110000, 0b00000000,
+  0b11100000, 0b00000000,
+  0b11000000, 0b00000000,
+  0b10000000, 0b00000000,
+  0b00000000, 0b00000000
+};
+byte StepTurn = 0;
+byte TurnIndex = 0;
+unsigned long TurnTime;
 void TaskTurn() {
   switch (StepTurn) {
     case 0:
@@ -157,8 +162,7 @@ void TaskTurn() {
 
     case 10:
       if (TurnTime <= millis()) {
-        if (TurnIndex < 40) {
-          //          TurnTime = millis() + TurnDelay;
+        if (TurnIndex < 48) {
           TurnTime += TurnDelay;
           SendLED(P0, TURN_0[TurnIndex * 2 + 1]);
           SendLED(P1, TURN_0[TurnIndex * 2]);
@@ -176,7 +180,13 @@ void TaskTurn() {
       break;
   }
 }
+// ------------- Конец эффекта поворотника
 
+
+
+// -------------- Автонастройка скорости эффекта поворотов
+byte StepAuto = 0;
+unsigned long AutoTime;
 void TaskAuto() {
   switch (StepAuto) {
     case 0:
@@ -189,14 +199,17 @@ void TaskAuto() {
       if ( (digitalRead(TURN) == HIGH) && ((AutoTime + 200) < millis())) {
         StepAuto = 20;
       }
+      else if ((AutoTime + 3000) < millis() ) StepAuto = 30;
       break;
     case 20:
       if (digitalRead(TURN) == LOW) {
-        TurnDelay += (((millis() - AutoTime) >> 6 ) - TurnDelay) >> 1;
+        TurnDelay += (((millis() - AutoTime) / 64 ) - TurnDelay) / 2;
+        //        TurnDelay += (((millis() - AutoTime) / 64) - TurnDelay) >> 1;
         AutoTime = millis();
         StepAuto = 10;
+        //        Serial.println(TurnDelay);
       }
-      else if ((AutoTime + 2000) < millis() ) {
+      else if ((AutoTime + 3000) < millis() ) {
         StepAuto = 30;
       }
       break;
@@ -206,12 +219,20 @@ void TaskAuto() {
       break;
   }
 }
+// -------- Конец автоподстройки
 
+
+
+// -------- Дневные ходовые огни + габариты
+byte StepDRL = 0;
+byte DRLPWM = 255;
+byte RealPWM = 255;
 void TaskDRL() {
   if (StepAuto != 0 ) {
     digitalWrite(PWM1, HIGH);
     DRLPWM = 255;
     RealPWM = 255;
+    StepPWM = 0;
   }
   else {
     if (digitalRead(PL) == LOW) DRLPWM = 223;
@@ -221,7 +242,12 @@ void TaskDRL() {
     }
   }
 }
+// ------- Конец ДХО и габаритов
 
+
+
+// -------- Изменения параметров ШИМ
+unsigned long PWMTime = 0;
 void TaskPWM() {
   switch (StepPWM) {
     case 0: if ( RealPWM != DRLPWM ) {
@@ -232,30 +258,38 @@ void TaskPWM() {
 
     case 10:
       if ( PWMTime < millis()) {
-        if (RealPWM > DRLPWM) RealPWM--;
-        else RealPWM++;
-
         if ( RealPWM == DRLPWM ) StepPWM = 0;
+        else {
+          if (RealPWM > DRLPWM) RealPWM--;
+          else RealPWM++;
 
-        switch (RealPWM) {
-          case 255: digitalWrite(PWM1, HIGH);
-            break;
+          switch (RealPWM) {
+            case 255: digitalWrite(PWM1, HIGH);
+              break;
 
-          case 0: digitalWrite(PWM1, LOW);
-            break;
+            case 0: digitalWrite(PWM1, LOW);
+              break;
 
-          default:
-            analogWrite(PWM1, RealPWM );
+            default:
+              analogWrite(PWM1, RealPWM );
+          }
+          PWMTime += 2;
+          //          Serial.println(RealPWM);
         }
-        PWMTime++;
       }
       break;
   }
 }
+// ------ Конец изменения ШИМ
 
+
+
+// ------ Эффекты
+byte StepEff = 0;
 void TaskEffects() {
   // Еффекты. Работают только если остальное не работает
-  if ((StepAuto == 0) && (digitalRead(PL) == HIGH) && (digitalRead(DRL) == HIGH)) {
+  //  if ((StepAuto == 0) && (digitalRead(PL) == HIGH) && (digitalRead(DRL) == HIGH)) {
+  if ((StepAuto == 0) && (RealPWM == 255)) {
     if (digitalRead(STROBE) == LOW || StepStrobe != 0 ) TaskStrobe();
     else {
       if (digitalRead(SHOW) == LOW ) {
@@ -270,13 +304,17 @@ void TaskEffects() {
     StepShow = 0;
   }
 }
+// ------ Конец эффектов
 
+
+
+// ------ Стробоскоп
 void TaskStrobe() {
   if (TimeStrobe < millis()) {
     switch (StepStrobe) {
       case 0:
-        StepStrobe = 10;
         TimeStrobe = millis();
+        StepStrobe = 10;
         break;
 
       case 10:
@@ -325,12 +363,20 @@ void TaskStrobe() {
     }
   }
 }
+// ----- Конец стробоскопа
 
 
+
+// ----- Демо режим
+unsigned long ShowTime = 0;
 void TaskShow() {
 
 }
+// ----- Конец демо
 
+
+
+// ----- Сброс параметров
 void OutputReset() {
   digitalWrite(PWM0, HIGH);
   digitalWrite(PWM1, HIGH);
@@ -340,4 +386,4 @@ void OutputReset() {
   SendLED(DRL0, 0xFF);
   digitalWrite(PWM0, LOW);
 }
-
+// ----- Конец сброса
