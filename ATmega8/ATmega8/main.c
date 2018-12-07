7970dd52c33fd57e18bff5d20859a8fcef77a98f
Flying Spaghetti Monster <36735901+giDai7ja@users.noreply.github.com>
2018-12-07 21:39:48 +0300

    minor

---
+++ ATmega8/ATmega8/main.c
@@ -0,0 +1,22 @@
+/*
+ * ATmega8.c
+ *
+ * Created: 24.11.2018 13:25:45
+ * Author : Пеппа
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	    DDRB = 0xff;  // PORTB as OUTPUT
+	    PORTB = 0x00;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
