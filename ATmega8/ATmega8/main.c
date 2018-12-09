657c235672e14f2f634ada5c35d268edabe2bdf0
Flying Spaghetti Monster <36735901+giDai7ja@users.noreply.github.com>
2018-12-09 22:14:27 +0300

    next

--- ATmega8/ATmega8/main.c
+++ ATmega8/ATmega8/main.c
@@ -1,22 +1,43 @@
 /*
- * ATmega8.c
- *
- * Created: 24.11.2018 13:25:45
- * Author : Пеппа
- */ 
+* ATmega8.c
+*
+* Created: 24.11.2018 13:25:45
+* Author : Пеппа
+*/
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+//#define F_CPU 8000000UL
+
+unsigned long int TIK;
+
+ISR(TIMER0_OVF_vect){
+	TIK++;
+}
+
+
+
+
 
 
 int main(void)
 {
-	    DDRB = 0xff;  // PORTB as OUTPUT
-	    PORTB = 0x00;
+	DDRB = 0xff;  // PORTB as OUTPUT
+	DDRD = 0xff;  // PORTB as OUTPUT
+	PORTB = 0x00;
+	PORTD = 0x00;
 	
-    /* Replace with your application code */
-    while (1) 
-    {
+	TCCR0 = (1<<CS00)|(1<<CS02);
+	TIMSK = (1<<TOIE0);
+	
+	
+	sei();
+	
+	/* Replace with your application code */
+	while (1)
+	{
 		
-    }
+	}
 }
 
